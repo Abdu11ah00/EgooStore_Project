412daaf10946de32658f72597d99c15c550b2d66
abdullah_alghamdi <abdullahalghmdi07@gmail.com>
2023-11-07 19:05:59 +0300

    I created “include_about.php” file which I wrote the page HTML code "Front-end". I also added our partner KSU.

--- Store/includes/include_partners.php
+++ Store/includes/include_partners.php
@@ -6,10 +6,43 @@
                 <h2>explore our products</h2>
             </div>
             <div class="row g-4">
+
+                <div class="col-12">
+                    <div class="accordion-item">
+                        <div class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <span class="accor-header-inner d-flex flex-wrap align-items-center">
+                                    <span class="accor-thumb">
+                                        <img style="width: 40px; height: 60px; object-fit: contain;"
+                                            src="assets/images/logo/ksu.png" alt="partner-thumb" />
+                                    </span>
+                                    <span class="accor-title">King Saud University</span>
+                                </span>
+                            </button>
+                        </div>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <p>
+                                    King Saud University (KSU) is a prominent public research university located in
+                                    Riyadh
+                                    the capital city of Saudi Arabia. It is one of the oldest and most prestigious
+                                    universities in the country
+                                    and is considered a leading institution of higher education in the Arab world.
+                                    KSU was established in 1957 and has since grown
+                                    into a comprehensive university with a diverse range of academic programs and
+                                    research activities.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="col-lg-6 col-12">
                     <div class="partner-list" id="accordionExample">
                         <div class="row g-4 justify-content-center">
-              
+
                             <div class="col-12">
                                 <div class="accordion-item">
                                     <div class="accordion-header" id="headingTwo">
@@ -28,12 +61,12 @@
                                         aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                         <div class="accordion-body">
                                             <p>
-                                            The people who built this server.
-                                            1.Abdullah Altayaar
-                                            2.Ibrahim Al-Jabri
-                                            3.Nawaf Al-Awda
-                                            4.abdullah_Alghamdi
-                                            5.Saad Al shaflut
+                                                The people who built this server.
+                                                1.Abdullah Altayaar
+                                                2.Ibrahim Al-Jabri
+                                                3.Nawaf Al-Awda
+                                                4.abdullah_Alghamdi
+                                                5.Saad Al shaflut
                                             </p>
                                         </div>
                                     </div>
@@ -54,7 +87,8 @@
                                             aria-expanded="true" aria-controls="collapseFive">
                                             <span class="accor-header-inner d-flex flex-wrap align-items-center">
                                                 <span class="accor-thumb">
-                                                    <img src="assets/images/sponsor/ArabCityCFW.png" alt="partner-thumb" />
+                                                    <img src="assets/images/sponsor/ArabCityCFW.png"
+                                                        alt="partner-thumb" />
                                                 </span>
                                                 <span class="accor-title">Arab City CFW</span>
                                             </span>
@@ -64,7 +98,7 @@
                                         aria-labelledby="headingFive" data-bs-parent="#accordionExample-2">
                                         <div class="accordion-body">
                                             <p>
-                                                 Arab City CFW FiveM server to gather people and play with others.
+                                                Arab City CFW FiveM server to gather people and play with others.
                                             </p>
                                         </div>
                                     </div>
